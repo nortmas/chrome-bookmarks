1f5cbfab8fd9412d3d96da18d19be19aea8a95a1
Dmitry Antonenko <raerayan@gmail.com>
2021-01-06 19:52:38 +0700

    Refactoring

--- ChromeBookmarks.py
+++ ChromeBookmarks.py
@@ -55,18 +55,16 @@ class ChromeBookmarks(Extension):
     def find_rec(self, data, query, matches):
 
         if self.matches_len >= self.max_matches_len:
-            return matches
+            return
 
         if data['type'] == 'folder':
             for child in data['children']:
                 self.find_rec(child, query, matches)
         else:
-            res = data['name'].lower().find(query, 0, len(data['name']))
+            res = data['name'].lower().find(query.lower())
             if res != -1:
                 matches.append(data)
                 self.matches_len += 1
-
-        return matches
 
     def get_items(self, query):
 
@@ -77,13 +75,13 @@ class ChromeBookmarks(Extension):
             query = ''
 
         for bookmarks_path, browser in self.bookmarks_paths:
+            
             matches = []
+            
             with open(bookmarks_path) as data_file:
                 data = json.load(data_file)
-                bookmark_bar = data['roots']['bookmark_bar']
-                other = data['roots']['other']
-                self.find_rec(other, query, matches)
-                matches = self.find_rec(bookmark_bar, query.lower(), matches)
+                self.find_rec(data['roots']['bookmark_bar'], query, matches)
+                self.find_rec(data['roots']['other'], query, matches)
 
             for bookmark in matches:
                 bookmark_name = bookmark['name'].encode('utf-8')
